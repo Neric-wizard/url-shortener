2e06a12fd357504cab49c418f7347a735685ef71
Njeck Neric <njeckneric507@gmail.com>
2026-03-25 22:46:14 +0100

    Fix redirect loop in URL shortener

--- app/[shortCode]/page.tsx
+++ app/[shortCode]/page.tsx
@@ -11,15 +11,25 @@ export default async function RedirectPage({
 }: {
   params: { shortCode: string };
 }) {
+  // Get the long URL from database
   const { data, error } = await supabase
     .from("links")
     .select("long_url")
     .eq("short_code", params.shortCode)
     .single();
 
-  if (error || !data) {
+  // If link doesn't exist, go to homepage
+  if (error || !data?.long_url) {
     redirect("/");
+    return null;
   }
 
-  redirect(data.long_url);
+  // Make sure the URL has http:// or https://
+  let longUrl = data.long_url;
+  if (!longUrl.startsWith("http://") && !longUrl.startsWith("https://")) {
+    longUrl = "https://" + longUrl;
+  }
+
+  // Redirect to the long URL
+  redirect(longUrl);
 }
